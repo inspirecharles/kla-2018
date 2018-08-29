dd74ce5989325fad7a275c895115c9bb9e18da4d
james-cabilino <james@inspireteam.com>
2018-08-29 17:59:59 +0800

    KLA-112: updates and fixes on mobile view

--- webui/src/shared/components/HeaderContainer.js
+++ webui/src/shared/components/HeaderContainer.js
@@ -14,7 +14,7 @@ class HeaderContainer extends Component {
 	                    <a className="logo" href="#"><img className="uk-logo" src="../../img/uk-logo.svg" /></a>
 	                </section>
 			        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			        	<span className="navbar-toggler-icon"></span>
+			        	<span className="navbar-toggler-icon"><img className="uk-logo" src="../../img/icons/menu.svg" /></span>
 			        </button>
 			        <div className="collapse navbar-collapse" id="navbarResponsive">
 			        	<ul className="navbar-nav ml-auto">
