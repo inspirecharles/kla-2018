3460201ddfcc5c48d01c90b40fe54d5b6613940d
james-cabilino <james@inspireteam.com>
2018-08-31 18:55:35 +0800

    KLA-112: fixes and adding font family

--- webui/src/shared/components/HomeContainer.js
+++ webui/src/shared/components/HomeContainer.js
@@ -32,7 +32,7 @@ class HomeContainer extends Component {
 				    		</div>
 				    	</div>
 				        <div className="row">
-						    {this.props.games.map((x, i) =>  {
+						    {[...Array(6)].map((x, i) =>  {
 						    		var show = (i == 0 || i == 1)?true:false;
 							    	return <HomeCardComponent key={i} show={show} name={"card"+i}/>
 						    	}
