894fe91dfeb1258e5da26853ece79bd726882395
inspirecharles <charles@inspireteam.com>
2018-09-11 10:59:30 +0800

    display next draw

--- webui/src/shared/components/home/HomeCardComponent.js
+++ webui/src/shared/components/home/HomeCardComponent.js
@@ -73,7 +73,7 @@ class HomeCardComponent extends Component {
 
 				            <div className="card-footer container">
 								<div className="row footerNextDraw">
-									<h3 className="nextDraw">Next Draw: &#163;7,000,000</h3>
+									<h3 className="nextDraw">Next Draw: {this.props.game.results && this.props.game.results[0] && decodeURIComponent(JSON.parse(this.props.game.results[0].next_jackpot))}</h3>
 									<button className="btn btn-light ml-auto">Buy Now</button>
 								</div>
 							</div>
