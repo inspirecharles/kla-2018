1d779823818f17f49cd8d2aa8c5674e56ec66714
inspirecharles <charles@inspireteam.com>
2018-10-05 10:53:55 +0800

    KLA-138: tracking tag to header

--- webui/src/server/index.js
+++ webui/src/server/index.js
@@ -79,6 +79,15 @@ function renderFullPage(html, preloadedState) {
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<title>Latest UK Lottery Results</title>
 				<link rel="stylesheet" type="text/css" href="/css/main.css">
+				<!-- Google Tag Manager -->
+				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+
+				{'gtm.start': new Date().getTime(),event:'gtm.js'}
+				);var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-MV45HPT');</script>
+				<!-- End Google Tag Manager -->
 			</head>
 			<body>
 				<div id="print-me"></div>
