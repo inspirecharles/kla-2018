a09969799fbdabcb85680812e13bc6626eff7fab
inspirecharles <charles@inspireteam.com>
2018-10-09 14:43:24 +0800

    View Prizes / Past results / Buy Now Buttons are not aligned

--- webui/src/shared/components/home/HomeCardComponent.js
+++ webui/src/shared/components/home/HomeCardComponent.js
@@ -94,7 +94,7 @@ class HomeCardComponent extends Component {
 
 	                        <div className="gameResults-actionLink">
 	                        	<div className="actionLink">
-	                            	<div className="supplementary_numbers col-lg-12 col-md-12">
+	                            	<div className="supplementary_numbers">
 	                            		<div className="supp-link-icons">
 	                            			<PrintComponent elem={this.props.game.slug+"-container"} />
 			                            	<ExportComponent game={this.props.game.slug} draw_id={this.props.game.results && this.props.game.results[0] && this.props.game.results[0].draw_id} />
